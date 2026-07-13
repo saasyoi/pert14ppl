b6ab148e57b643bd694b552eac1179b3a7c2ac82
saasyoi <sulthanaidhanssn@gmail.com>
2026-07-13 10:26:45 +0700

    Update hello_aut.py

--- tests/hello_aut.py
+++ tests/hello_aut.py
@@ -1,15 +1,20 @@
 import unittest, sys
+import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 class AutTest(unittest.TestCase):
     def setUp(self):
-        options = webdriver.FirefoxOptions()
-        options.add_argument('--ignore-ssl-errors=yes')
-        options.add_argument('--ignore-certificate-errors')
+        browser_type = os.getenv('BROWSER_NAME', 'firefox')
+        if browser_type == 'chrome':
+            options = webdriver.ChromeOptions()
+        elif browser_type == 'edge':
+            options = webdriver.EdgeOptions()
+        else:
+            options = webdriver.FirefoxOptions()
+
         server = 'http://localhost:4444'
         self.browser = webdriver.Remote(command_executor=server, options=options)
-        self.addCleanup(self.browser.quit)
 
     def test_homepage(self):
         if len(sys.argv) > 1:
